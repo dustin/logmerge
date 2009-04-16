5e1a794101686533cddd9b1672373d397e62d2c5
Dustin Sallings <dustin@spy.net>
2009-04-16 09:30:55 -0700

    Reformatted files.

--- logfiles.cpp
+++ logfiles.cpp
@@ -27,94 +27,94 @@
 #define NOTREACHED 0
 
 #define AMAZON_S3_REGEX "^[0-9a-f]+ ([-A-z0-9_\\.]+) \\[(.*)\\] ([0-9\\.]+) " \
-	"[0-9a-f]+ [0-9A-F]+ \\S+ \\S+ (\"[^\"]*\") (\\d+) [-A-z0-9]+ ([-0-9]+) " \
-	"[-0-9]+ \\d+ [-0-9]+ (\"[^\"]*\") (\"[^\"]*\")"
+    "[0-9a-f]+ [0-9A-F]+ \\S+ \\S+ (\"[^\"]*\") (\\d+) [-A-z0-9]+ ([-0-9]+) " \
+    "[-0-9]+ \\d+ [-0-9]+ (\"[^\"]*\") (\"[^\"]*\")"
 
 boost::regex amazon_s3_regex(AMAZON_S3_REGEX, boost::regex::perl);
 
 static bool myGzgets(struct logfile *lf)
 {
-	char *rv=lf->line;
-	int s=LINE_BUFFER;
-	int bytesRead=0;
+    char *rv=lf->line;
+    int s=LINE_BUFFER;
+    int bytesRead=0;
 
-	lf->lineLength=0;
+    lf->lineLength=0;
 
-	for(;;) {
-		if(lf->gzBufCur > lf->gzBufEnd || lf->gzBufEnd == NULL) {
-			/* Fetch some more stuff */
-			bytesRead=gzread(lf->input, lf->gzBuf, GZBUFFER);
-			lf->gzBufEnd=bytesRead + lf->gzBuf - 1;
-			/* Make sure we got something */
-			if(bytesRead == 0) {
-				return(false);
-			}
-			lf->gzBufCur=lf->gzBuf;
-		}
-		/* Make sure we do not get too many characters */
-		if(--s > 0) {
-			*rv++ = *lf->gzBufCur;
-			lf->lineLength++;
-			if(*(lf->gzBufCur++) == '\n') {
-				*rv=0x00;
-				return(true);
-			}
-		} else {
-			*rv=0x00;
-			return(true);
-		}
-	}
+    for(;;) {
+        if(lf->gzBufCur > lf->gzBufEnd || lf->gzBufEnd == NULL) {
+            /* Fetch some more stuff */
+            bytesRead=gzread(lf->input, lf->gzBuf, GZBUFFER);
+            lf->gzBufEnd=bytesRead + lf->gzBuf - 1;
+            /* Make sure we got something */
+            if(bytesRead == 0) {
+                return(false);
+            }
+            lf->gzBufCur=lf->gzBuf;
+        }
+        /* Make sure we do not get too many characters */
+        if(--s > 0) {
+            *rv++ = *lf->gzBufCur;
+            lf->lineLength++;
+            if(*(lf->gzBufCur++) == '\n') {
+                *rv=0x00;
+                return(true);
+            }
+        } else {
+            *rv=0x00;
+            return(true);
+        }
+    }
 
-	assert(NOTREACHED);
-	return(rv);
+    assert(NOTREACHED);
+    return(rv);
 }
 
 /* Returns a value from logTypes */
 static enum logType identifyLog(const char *line) {
-	enum logType rv=UNKNOWN;
-	assert(line != NULL);
+    enum logType rv=UNKNOWN;
+    assert(line != NULL);
 
-	if(boost::regex_search(line, amazon_s3_regex)) {
-		rv=AMAZON_S3;
-	} else {
-		rv=COMMON;
-	}
-	return rv;
+    if(boost::regex_search(line, amazon_s3_regex)) {
+        rv=AMAZON_S3;
+    } else {
+        rv=COMMON;
+    }
+    return rv;
 }
 
 static void outputLineS3(struct logfile *lf) {
-	boost::cmatch what;
+    boost::cmatch what;
 
-	assert(lf);
-	assert(lf->line);
+    assert(lf);
+    assert(lf->line);
 
-/*
-// Positions as defined in the regex
-S3_BUCKET	1
-S3_DATE		2
-S3_IP		3
-S3_REQ		4
-S3_STATUS	5
-S3_SIZE		6
-S3_REFER	7
-S3_UA		8
-*/
+    /*
+    // Positions as defined in the regex
+    S3_BUCKET   1
+    S3_DATE     2
+    S3_IP       3
+    S3_REQ      4
+    S3_STATUS   5
+    S3_SIZE     6
+    S3_REFER    7
+    S3_UA       8
+    */
 
-	if(boost::regex_search(lf->line, what, amazon_s3_regex)) {
-		std::ostream_iterator<char> out(std::cout);
-		what.format(out, "$3 - - [$2] $4 $5 $6 $7 $8 $1\n");
-	} else {
-		fprintf(stderr, "*** S3: Failed to match ``%s''\n", lf->line);
-	}
+    if(boost::regex_search(lf->line, what, amazon_s3_regex)) {
+        std::ostream_iterator<char> out(std::cout);
+        what.format(out, "$3 - - [$2] $4 $5 $6 $7 $8 $1\n");
+    } else {
+        fprintf(stderr, "*** S3: Failed to match ``%s''\n", lf->line);
+    }
 }
 
 static void outputLineDirect(struct logfile *lf) {
-	assert(lf != NULL);
-	assert(lf->line != NULL);
-	if(fwrite(lf->line, lf->lineLength, 1, stdout) < lf->lineLength) {
-		perror("fwrite");
-		exit(EX_IOERR);
-	}
+    assert(lf != NULL);
+    assert(lf->line != NULL);
+    if(fwrite(lf->line, lf->lineLength, 1, stdout) < lf->lineLength) {
+        perror("fwrite");
+        exit(EX_IOERR);
+    }
 }
 
 /**
@@ -122,39 +122,39 @@ static void outputLineDirect(struct logfile *lf) {
  */
 int openLogfile(struct logfile *lf)
 {
-	int rv=ERROR;
-	assert(lf != NULL);
+    int rv=ERROR;
+    assert(lf != NULL);
 
-	assert(! lf->isOpen);
+    assert(! lf->isOpen);
 
-	fprintf(stderr, "*** Opening %s\n", lf->filename);
+    fprintf(stderr, "*** Opening %s\n", lf->filename);
 
-	lf->input=gzopen(lf->filename, "r");
+    lf->input=gzopen(lf->filename, "r");
 
-	if(lf->input != NULL) {
-		lf->isOpen=true;
-		rv=OK;
-	}
+    if(lf->input != NULL) {
+        lf->isOpen=true;
+        rv=OK;
+    }
 
-	/* Allocate the line buffer */
-	lf->line=(char*)calloc(1, LINE_BUFFER);
-	assert(lf->line != NULL);
-	lf->lineLength=0;
+    /* Allocate the line buffer */
+    lf->line=(char*)calloc(1, LINE_BUFFER);
+    assert(lf->line != NULL);
+    lf->lineLength=0;
 
-	/* Allocate the read buffer */
-	lf->gzBuf=(char*)calloc(1, GZBUFFER);
-	assert(lf->gzBuf != NULL);
+    /* Allocate the read buffer */
+    lf->gzBuf=(char*)calloc(1, GZBUFFER);
+    assert(lf->gzBuf != NULL);
 
-	lf->gzBufCur=NULL;
-	lf->gzBufEnd=NULL;
+    lf->gzBufCur=NULL;
+    lf->gzBufEnd=NULL;
 
-	return(rv);
+    return(rv);
 }
 
 /* A date and a string */
 struct date_str {
-	char *datestr;
-	int val;
+    char *datestr;
+    int val;
 };
 
 #define MONTH_JAN       (((((('J'<<8)|'a')<<8)|'n')<<8)|'/')
@@ -173,106 +173,106 @@ struct date_str {
 /* Convert a three character month to the numeric value */
 TESTED_STATIC int parseMonth(const char *input) {
     int rv=-1;
-	int inputInt=0;
+    int inputInt=0;
 
-	for(int i=0; i<4 && input[i]; i++) {
-		inputInt = (inputInt << 8) | input[i];
-	}
+    for(int i=0; i<4 && input[i]; i++) {
+        inputInt = (inputInt << 8) | input[i];
+    }
 
-	switch(inputInt) {
-		case MONTH_JAN: rv=0; break;
-		case MONTH_FEB: rv=1; break;
-		case MONTH_MAR: rv=2; break;
-		case MONTH_APR: rv=3; break;
-		case MONTH_MAY: rv=4; break;
-		case MONTH_JUN: rv=5; break;
-		case MONTH_JUL: rv=6; break;
-		case MONTH_AUG: rv=7; break;
-		case MONTH_SEP: rv=8; break;
-		case MONTH_OCT: rv=9; break;
-		case MONTH_NOV: rv=10; break;
-		case MONTH_DEC: rv=11; break;
-	}
+    switch(inputInt) {
+    case MONTH_JAN: rv=0; break;
+    case MONTH_FEB: rv=1; break;
+    case MONTH_MAR: rv=2; break;
+    case MONTH_APR: rv=3; break;
+    case MONTH_MAY: rv=4; break;
+    case MONTH_JUN: rv=5; break;
+    case MONTH_JUL: rv=6; break;
+    case MONTH_AUG: rv=7; break;
+    case MONTH_SEP: rv=8; break;
+    case MONTH_OCT: rv=9; break;
+    case MONTH_NOV: rv=10; break;
+    case MONTH_DEC: rv=11; break;
+    }
 
-	return rv;
+    return rv;
 }
 
 class BadTimestamp : public std::exception {
-	virtual const char* what() const throw() {
-		return "Timestamp parse error";
-	}
+    virtual const char* what() const throw() {
+        return "Timestamp parse error";
+    }
 };
 
 static time_t parseTimestamp(struct logfile *lf)
 {
-	char *p;
+    char *p;
 
-	assert(lf != NULL);
-	assert(lf->line != NULL);
+    assert(lf != NULL);
+    assert(lf->line != NULL);
 
-	lf->timestamp=-1;
+    lf->timestamp=-1;
 
-	p=lf->line;
+    p=lf->line;
 
-	try {
+    try {
 
-		/* The shortest line I can parse is about 32 characters. */
-		if(lf->lineLength < 32) {
-			/* This is a broken entry */
-			fprintf(stderr, "Broken log entry (too short):  %s\n", p);
-		} else if(index(p, '[') != NULL) {
-			struct tm tm;
-			memset(&tm, 0x00, sizeof(tm));
+        /* The shortest line I can parse is about 32 characters. */
+        if(lf->lineLength < 32) {
+            /* This is a broken entry */
+            fprintf(stderr, "Broken log entry (too short):  %s\n", p);
+        } else if(index(p, '[') != NULL) {
+            struct tm tm;
+            memset(&tm, 0x00, sizeof(tm));
 
-			p=index(p, '[');
-			/* Input validation */
-			if(p == NULL || lf->lineLength < 32) {
-				fprintf(stderr, "invalid log line:  %s\n", lf->line);
-				throw BadTimestamp();
-			}
+            p=index(p, '[');
+            /* Input validation */
+            if(p == NULL || lf->lineLength < 32) {
+                fprintf(stderr, "invalid log line:  %s\n", lf->line);
+                throw BadTimestamp();
+            }
 
-			/* fprintf(stderr, "**** Parsing %s\n", p); */
-			p++;
-			tm.tm_mday=atoi(p);
-			p+=3;
-			tm.tm_mon=parseMonth(p);
-			p+=4;
-			tm.tm_year=atoi(p);
-			p+=5;
-			tm.tm_hour=atoi(p);
-			p+=3;
-			tm.tm_min=atoi(p);
-			p+=3;
-			tm.tm_sec=atoi(p);
+            /* fprintf(stderr, "**** Parsing %s\n", p); */
+            p++;
+            tm.tm_mday=atoi(p);
+            p+=3;
+            tm.tm_mon=parseMonth(p);
+            p+=4;
+            tm.tm_year=atoi(p);
+            p+=5;
+            tm.tm_hour=atoi(p);
+            p+=3;
+            tm.tm_min=atoi(p);
+            p+=3;
+            tm.tm_sec=atoi(p);
 
-			/* Make sure it still looks like CLF */
-			if(p[2] != ' ') {
-				fprintf(stderr,
-					"log line is starting to not look like CLF: %s\n",
-					lf->line);
-				throw BadTimestamp();
-			}
+            /* Make sure it still looks like CLF */
+            if(p[2] != ' ') {
+                fprintf(stderr,
+                        "log line is starting to not look like CLF: %s\n",
+                        lf->line);
+                throw BadTimestamp();
+            }
 
-			tm.tm_year-=1900;
+            tm.tm_year-=1900;
 
-			/* Let mktime guess the timezone */
-			tm.tm_isdst=-1;
+            /* Let mktime guess the timezone */
+            tm.tm_isdst=-1;
 
-			lf->timestamp=mktime(&tm);
+            lf->timestamp=mktime(&tm);
 
-		} else {
-			fprintf(stderr, "Unknown log format:  %s\n", p);
-		}
+        } else {
+            fprintf(stderr, "Unknown log format:  %s\n", p);
+        }
 
-	} catch(BadTimestamp e) {
-		// Damn.
-	}
+    } catch(BadTimestamp e) {
+        // Damn.
+    }
 
-	if(lf->timestamp < 0) {
-		fprintf(stderr, "* Error parsing timestamp from %s", lf->line);
-	}
+    if(lf->timestamp < 0) {
+        fprintf(stderr, "* Error parsing timestamp from %s", lf->line);
+    }
 
-	return(lf->timestamp);
+    return(lf->timestamp);
 }
 
 /**
@@ -281,72 +281,72 @@ static time_t parseTimestamp(struct logfile *lf)
  */
 static bool nextLine(struct logfile *lf)
 {
-	bool rv=false;
+    bool rv=false;
 
-	assert(lf != NULL);
+    assert(lf != NULL);
 
-	if(!lf->isOpen) {
-		int logfileOpened=openLogfile(lf);
-		/* This looks a little awkward, but it's the only way I can both
-		 * avoid the side effect of having assert perform the task and
-		 * not leave the variable unreferenced when assertions are off.
-		 */
-		if(logfileOpened != OK) {
-			assert(logfileOpened == OK);
-		}
-		/* Recurse to skip a line */
-		rv=nextLine(lf);
-		assert(rv);
-	}
+    if(!lf->isOpen) {
+        int logfileOpened=openLogfile(lf);
+        /* This looks a little awkward, but it's the only way I can both
+         * avoid the side effect of having assert perform the task and
+         * not leave the variable unreferenced when assertions are off.
+         */
+        if(logfileOpened != OK) {
+            assert(logfileOpened == OK);
+        }
+        /* Recurse to skip a line */
+        rv=nextLine(lf);
+        assert(rv);
+    }
 
-	if(myGzgets(lf)) {
-		rv=true;
-		char *p=lf->line;
-		/* Make sure the line is short enough */
-		assert(lf->lineLength < LINE_BUFFER);
-		/* Make sure we read a line */
-		if(p[lf->lineLength-1] != '\n') {
-			fprintf(stderr, "*** BROKEN LOG ENTRY IN %s (no newline)\n",
-				lf->filename);
-			rv=false;
-		} else if(parseTimestamp(lf) == -1) {
-			/* If we can't parse the timestamp, give up */
-			rv=false;
-		}
-	}
+    if(myGzgets(lf)) {
+        rv=true;
+        char *p=lf->line;
+        /* Make sure the line is short enough */
+        assert(lf->lineLength < LINE_BUFFER);
+        /* Make sure we read a line */
+        if(p[lf->lineLength-1] != '\n') {
+            fprintf(stderr, "*** BROKEN LOG ENTRY IN %s (no newline)\n",
+                    lf->filename);
+            rv=false;
+        } else if(parseTimestamp(lf) == -1) {
+            /* If we can't parse the timestamp, give up */
+            rv=false;
+        }
+    }
 
-	return rv;
+    return rv;
 }
 
 static void closeLogfile(struct logfile *lf)
 {
-	int gzerrno=0;
+    int gzerrno=0;
 
-	assert(lf != NULL);
-	assert(lf->input != NULL);
-	assert(lf->filename != NULL);
+    assert(lf != NULL);
+    assert(lf->input != NULL);
+    assert(lf->filename != NULL);
 
-	fprintf(stderr, "*** Closing %s\n", lf->filename);
+    fprintf(stderr, "*** Closing %s\n", lf->filename);
 
-	/* Free the line buffer */
-	if(lf->line != NULL) {
-		free(lf->line);
-		lf->line=NULL;
-	}
+    /* Free the line buffer */
+    if(lf->line != NULL) {
+        free(lf->line);
+        lf->line=NULL;
+    }
 
-	gzerrno=gzclose(lf->input);
-	if(gzerrno!=0) {
-		gzerror(lf->input, &gzerrno);
-	}
-	lf->isOpen=false;
+    gzerrno=gzclose(lf->input);
+    if(gzerrno!=0) {
+        gzerror(lf->input, &gzerrno);
+    }
+    lf->isOpen=false;
 
-	if(lf->gzBuf != NULL) {
-		free(lf->gzBuf);
-		lf->gzBuf = NULL;
-	}
+    if(lf->gzBuf != NULL) {
+        free(lf->gzBuf);
+        lf->gzBuf = NULL;
+    }
 
-	lf->gzBufCur=NULL;
-	lf->gzBufEnd=NULL;
+    lf->gzBufCur=NULL;
+    lf->gzBufEnd=NULL;
 }
 
 /**
@@ -354,27 +354,27 @@ static void closeLogfile(struct logfile *lf)
  */
 static void destroyLogfile(struct logfile *lf)
 {
-	assert(lf != NULL);
+    assert(lf != NULL);
 
-	fprintf(stderr, "** Destroying %s\n", lf->filename);
+    fprintf(stderr, "** Destroying %s\n", lf->filename);
 
-	if(lf->isOpen) {
-		closeLogfile(lf);
-	}
+    if(lf->isOpen) {
+        closeLogfile(lf);
+    }
 
-	/* Free the parts */
-	if(lf->filename!=NULL) {
-		free(lf->filename);
-	}
-	if(lf->line != NULL) {
-		free(lf->line);
-	}
-	if(lf->gzBuf != NULL) {
-		free(lf->gzBuf);
-	}
+    /* Free the parts */
+    if(lf->filename!=NULL) {
+        free(lf->filename);
+    }
+    if(lf->line != NULL) {
+        free(lf->line);
+    }
+    if(lf->gzBuf != NULL) {
+        free(lf->gzBuf);
+    }
 
-	/* Lastly, free the container itself. */
-	free(lf);
+    /* Lastly, free the container itself. */
+    free(lf);
 }
 
 /**
@@ -382,52 +382,52 @@ static void destroyLogfile(struct logfile *lf)
  */
 struct logfile *createLogfile(const char *filename)
 {
-	struct logfile *rv=NULL;
+    struct logfile *rv=NULL;
 
-	rv=(struct logfile *)calloc(1, sizeof(struct logfile));
-	assert(rv != NULL);
+    rv=(struct logfile *)calloc(1, sizeof(struct logfile));
+    assert(rv != NULL);
 
-	rv->filename=(char *)strdup(filename);
-	assert(rv->filename != NULL);
+    rv->filename=(char *)strdup(filename);
+    assert(rv->filename != NULL);
 
-	/* Try to open the logfile */
-	if(openLogfile(rv) != OK) {
-		destroyLogfile(rv);
-		rv=NULL;
-	} else {
-		/* If it's opened succesfully, read the next (first) line */
-		if(!nextLine(rv)) {
-			/* If nextLine didn't return a record, this entry is invalid. */
-			destroyLogfile(rv);
-			rv=NULL;
-		} else {
-			/* Otherwise, it's valid and we'll proceed, but close it. */
-			switch(identifyLog(rv->line)) {
-				case COMMON:
-					fprintf(stderr, "**** %s is a common log file\n", filename);
-					rv->outputLine=outputLineDirect;
-					break;
-				case AMAZON_S3:
-					fprintf(stderr, "**** %s is an s3 log file\n", filename);
-					rv->outputLine=outputLineS3;
-					break;
-				case UNKNOWN:
-					fprintf(stderr, "! Can't identify type of %s\n", filename);
-					break;
-				default:
-					assert(false);
-			}
+    /* Try to open the logfile */
+    if(openLogfile(rv) != OK) {
+        destroyLogfile(rv);
+        rv=NULL;
+    } else {
+        /* If it's opened succesfully, read the next (first) line */
+        if(!nextLine(rv)) {
+            /* If nextLine didn't return a record, this entry is invalid. */
+            destroyLogfile(rv);
+            rv=NULL;
+        } else {
+            /* Otherwise, it's valid and we'll proceed, but close it. */
+            switch(identifyLog(rv->line)) {
+            case COMMON:
+                fprintf(stderr, "**** %s is a common log file\n", filename);
+                rv->outputLine=outputLineDirect;
+                break;
+            case AMAZON_S3:
+                fprintf(stderr, "**** %s is an s3 log file\n", filename);
+                rv->outputLine=outputLineS3;
+                break;
+            case UNKNOWN:
+                fprintf(stderr, "! Can't identify type of %s\n", filename);
+                break;
+            default:
+                assert(false);
+            }
 
-			if(rv->outputLine == NULL) {
-				destroyLogfile(rv);
-				rv=NULL;
-			} else {
-				closeLogfile(rv);
-			}
-		}
-	}
+            if(rv->outputLine == NULL) {
+                destroyLogfile(rv);
+                rv=NULL;
+            } else {
+                closeLogfile(rv);
+            }
+        }
+    }
 
-	return(rv);
+    return(rv);
 }
 
 /**
@@ -436,16 +436,16 @@ struct logfile *createLogfile(const char *filename)
  */
 void skipRecord(log_queue& queue)
 {
-	struct logfile *oldEntry=NULL;
-	assert(!queue.empty());
+    struct logfile *oldEntry=NULL;
+    assert(!queue.empty());
 
-	oldEntry=queue.top();
-	queue.pop();
+    oldEntry=queue.top();
+    queue.pop();
 
-	/* If stuff comes back, reinsert the old entry */
-	if(nextLine(oldEntry)) {
-		queue.push(oldEntry);
-	} else {
-		destroyLogfile(oldEntry);
-	}
+    /* If stuff comes back, reinsert the old entry */
+    if(nextLine(oldEntry)) {
+        queue.push(oldEntry);
+    } else {
+        destroyLogfile(oldEntry);
+    }
 }
--- logmerge.cpp
+++ logmerge.cpp
@@ -17,86 +17,86 @@
 #define STDOUT_BUF_SIZE 1024*1024
 
 namespace logmerge {
-	static void initLogfiles(log_queue&, int, char **);
-	static void outputLogfiles(log_queue&);
-	static void initLogfile(log_queue&, const char*);
+    static void initLogfiles(log_queue&, int, char **);
+    static void outputLogfiles(log_queue&);
+    static void initLogfile(log_queue&, const char*);
 }
 
 static void logmerge::initLogfile(log_queue& queue, const char *filename) {
-	struct logfile *lf=createLogfile(filename);
-	if(lf!=NULL) {
-		queue.push(lf);
-	} else {
-		std::cerr << "Error opening logfile ``" << filename
-			<< "''" << std::endl;
-	}
+    struct logfile *lf=createLogfile(filename);
+    if(lf!=NULL) {
+        queue.push(lf);
+    } else {
+        std::cerr << "Error opening logfile ``" << filename
+                  << "''" << std::endl;
+    }
 }
 
 /* Initialize all of the logfiles */
 static void logmerge::initLogfiles(log_queue& queue, int argc, char **argv)
 {
-	if(argc>1) {
-		for(int i=1; i<argc; i++) {
-			initLogfile(queue, argv[i]);
-		}
-	} else {
-		char buf[8192];
-		std::cerr << "No logfiles given, accepting list from stdin"
-			<< std::endl;
-		while(fgets((char*)&buf, sizeof(buf)-1, stdin)) {
-			buf[strlen(buf)-1]=0x00;
-			initLogfile(queue, buf);
-		}
-	}
+    if(argc>1) {
+        for(int i=1; i<argc; i++) {
+            initLogfile(queue, argv[i]);
+        }
+    } else {
+        char buf[8192];
+        std::cerr << "No logfiles given, accepting list from stdin"
+                  << std::endl;
+        while(fgets((char*)&buf, sizeof(buf)-1, stdin)) {
+            buf[strlen(buf)-1]=0x00;
+            initLogfile(queue, buf);
+        }
+    }
 }
 
 static void logmerge::outputLogfiles(log_queue& queue)
 {
-	int entries=0;
-	struct logfile *lf=NULL;
+    int entries=0;
+    struct logfile *lf=NULL;
 
-	while(!queue.empty()) {
-		entries++;
+    while(!queue.empty()) {
+        entries++;
 
-		lf=queue.top();
-		assert(lf!=NULL);
-		if(! lf->isOpen) {
-			openLogfile(lf);
-		}
+        lf=queue.top();
+        assert(lf!=NULL);
+        if(! lf->isOpen) {
+            openLogfile(lf);
+        }
 
-		lf->outputLine(lf);
-		skipRecord(queue);
-	}
+        lf->outputLine(lf);
+        skipRecord(queue);
+    }
 
-	std::cerr << "Read " << entries << " entries" << std::endl;
+    std::cerr << "Read " << entries << " entries" << std::endl;
 }
 
 #ifdef USE_ASSERT
 static void testMonthParsing() {
-	char *months[] = {
-		"Jan/", "Feb/", "Mar/", "Apr/", "May/", "Jun/",
-		"Jul/", "Aug/", "Sep/", "Oct/", "Nov/", "Dec/"
-	};
-	for(int i=0; i<12; i++) {
-		for(int j=0; j<10; j++) {
-			int rv=parseMonth(months[i]);
-			if(i != rv) {
-				std::cerr << "Expected " << i << " for "
-					<< months[i] << " got " << rv << std::endl;
-				abort();
-			}
-		}
-	}
-	for(int j=0; j<10; j++) {
-		for(int i=0; i<12; i++) {
-			int rv=parseMonth(months[i]);
-			if(i != rv) {
-				std::cerr << "Expected " << i << " for "
-					<< months[i] << " got " << rv << std::endl;
-				abort();
-			}
-		}
-	}
+    char *months[] = {
+        "Jan/", "Feb/", "Mar/", "Apr/", "May/", "Jun/",
+        "Jul/", "Aug/", "Sep/", "Oct/", "Nov/", "Dec/"
+    };
+    for(int i=0; i<12; i++) {
+        for(int j=0; j<10; j++) {
+            int rv=parseMonth(months[i]);
+            if(i != rv) {
+                std::cerr << "Expected " << i << " for "
+                          << months[i] << " got " << rv << std::endl;
+                abort();
+            }
+        }
+    }
+    for(int j=0; j<10; j++) {
+        for(int i=0; i<12; i++) {
+            int rv=parseMonth(months[i]);
+            if(i != rv) {
+                std::cerr << "Expected " << i << " for "
+                          << months[i] << " got " << rv << std::endl;
+                abort();
+            }
+        }
+    }
 }
 #else
 static void testMonthParsing() {
@@ -108,14 +108,14 @@ static void testMonthParsing() {
  */
 int main(int argc, char **argv)
 {
-	log_queue queue;
+    log_queue queue;
 
-	testMonthParsing();
+    testMonthParsing();
 
-	setvbuf(stdout, NULL, _IOFBF, STDOUT_BUF_SIZE);
+    setvbuf(stdout, NULL, _IOFBF, STDOUT_BUF_SIZE);
 
-	logmerge::initLogfiles(queue, argc, argv);
-	logmerge::outputLogfiles(queue);
+    logmerge::initLogfiles(queue, argc, argv);
+    logmerge::outputLogfiles(queue);
 
-	return(0);
+    return(0);
 }
