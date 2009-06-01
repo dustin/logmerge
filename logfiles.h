f50c2418ec00f625dc65621c2c328557b9bc79d6
Dustin Sallings <dustin@spy.net>
2009-05-31 17:39:23 -0700

    Don't 'extern C' the whole header.

--- logfiles.h
+++ logfiles.h
@@ -28,58 +28,54 @@
  * helps. */
 #define GZBUFFER 1024*1024
 
-extern "C" {
+enum logType {
+    COMMON, AMAZON_S3, UNKNOWN
+};
 
-    enum logType {
-        COMMON, AMAZON_S3, UNKNOWN
-    };
+/* The logfile itself */
+struct logfile {
+    /* The filename of this log entry */
+    char *filename;
+    /* The current record */
+    char *line;
+    /* Look!  I know pascal! */
+    size_t lineLength;
+    /* Function to output the current line */
+    void (*outputLine)(struct logfile *);
+    /* The timestamp of the current record */
+    time_t timestamp;
+    /* Indicate whether this logfile is open */
+    bool isOpen;
+    /* Buffering for speeding up gzipped file access */
+    char *gzBufCur;
+    char *gzBufEnd;
+    char *gzBuf;
+    /* The actual input stream being read */
+    gzFile input;
+};
 
-    /* The logfile itself */
-    struct logfile {
-        /* The filename of this log entry */
-        char *filename;
-        /* The current record */
-        char *line;
-        /* Look!  I know pascal! */
-        size_t lineLength;
-        /* Function to output the current line */
-        void (*outputLine)(struct logfile *);
-        /* The timestamp of the current record */
-        time_t timestamp;
-        /* Indicate whether this logfile is open */
-        bool isOpen;
-        /* Buffering for speeding up gzipped file access */
-        char *gzBufCur;
-        char *gzBufEnd;
-        char *gzBuf;
-        /* The actual input stream being read */
-        gzFile input;
-    };
+class TimeCmp {
+ public:
+    bool operator() (const struct logfile* a, const struct logfile* b) const {
+        return a->timestamp > b->timestamp;
+    }
+};
 
-    class TimeCmp {
-    public:
-        bool operator() (const struct logfile* a, const struct logfile* b) const {
-            return a->timestamp > b->timestamp;
-        }
-    };
+typedef std::priority_queue<struct logfile *,
+    std::vector<struct logfile *>, TimeCmp>
+    log_queue;
 
-    typedef std::priority_queue<struct logfile *,
-        std::vector<struct logfile *>, TimeCmp>
-        log_queue;
+/* Get a new logfile */
+struct logfile *createLogfile(const char *filename);
+/* Skip to the next record in the list */
+void skipRecord(log_queue&);
+/* Open a logfile */
+int openLogfile(struct logfile *lf);
 
-    /* Get a new logfile */
-    struct logfile *createLogfile(const char *filename);
-    /* Skip to the next record in the list */
-    void skipRecord(log_queue&);
-    /* Open a logfile */
-    int openLogfile(struct logfile *lf);
-
-    /* Parse a month.  This is generally static, but exposed when assertions are
-       enabled. */
+/* Parse a month.  This is generally static, but exposed when assertions are
+   enabled. */
 #ifdef USE_ASSERT
-    TESTED_STATIC int parseMonth(const char*);
+TESTED_STATIC int parseMonth(const char*);
 #endif
-
-} // extern C
 
 #endif /* LOGFILES_H */
