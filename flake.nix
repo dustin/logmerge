77fd1ac92f39d1ab97f1da4bd9d8534ac7bdbe92
Dustin Sallings <dustin@spy.net>
2025-01-19 08:54:28 -1000

    Use nix flakes instead of a shell

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Logmerge";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }: 
+    let
+      eachSystem = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+    in {
+      packages = eachSystem (system: let pkgs = import nixpkgs { inherit system; }; in {
+        default = pkgs.stdenv.mkDerivation {
+          name = "logmerge";
+          src = ./.;
+          buildInputs = [ pkgs.zig ];
+        };
+      });
+
+      devShells = eachSystem (system: let pkgs = import nixpkgs { inherit system; }; in {
+        default = pkgs.mkShell {
+          buildInputs = [ pkgs.gnumake pkgs.boost ];
+        };
+      });
+    };
+}
--- shell.nix
+++
@@ -1,8 +0,0 @@
-with import <nixpkgs> {};
-
-stdenv.mkDerivation {
-  name = "logmerge";
-  buildInputs = [
-    gnumake boost
-  ];
-}
