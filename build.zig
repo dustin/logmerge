fb03d551628bba941821c9987b15b3edf4e3f87f
Dustin Sallings <dustin@spy.net>
2025-09-07 21:14:00 -1000

    Simplify build.zig

--- build.zig
+++ build.zig
@@ -33,28 +33,10 @@ pub fn build(b: *std.Build) void {
     });
     b.installArtifact(exe);
 
-    const run_cmd = b.addRunArtifact(exe);
-
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
-    const lib_unit_tests = b.addTest(.{
-        .root_module = lib,
-    });
-
+    const lib_unit_tests = b.addTest(.{ .root_module = lib });
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-    lib_unit_tests.root_module.addImport("zeit", zeit.module("zeit"));
 
-    const exe_unit_tests = b.addTest(.{
-        .root_module = exe.root_module,
-    });
-
+    const exe_unit_tests = b.addTest(.{ .root_module = exe.root_module });
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
     const test_step = b.step("test", "Run unit tests");
